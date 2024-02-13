828ee08c439beffc5fa180a1aae5a249b3148904
Reza Fajri Fikri <rezafajrifikri@gmail.com>
2024-02-13 12:32:49 +0700

    Initial commit

---
+++ assets/js/portfolio.js
@@ -0,0 +1,20 @@
+(function () {
+  "use strict";
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+})()
